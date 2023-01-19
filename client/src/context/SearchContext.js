7601bcbe769a87489348515c13b81b64792c1aad
ramishshah93 <ramishshah93@gmail.com>
2023-01-19 23:09:20 +0100

    AUTHcontextprovider custom hook ready to use

--- client/src/context/SearchContext.js
+++ client/src/context/SearchContext.js
@@ -1,3 +1,5 @@
+import { createContext, useReducer } from "react";
+
 const INITIAL_STATE = {
   city: undefined,
   dates: [],
@@ -13,6 +15,7 @@ export const SearchContext = createContext(INITIAL_STATE);
 const SearchReducer = (state, action) => {
   switch (action.type) {
     case "NEW_SEARCH":
+      console.log("action.payload SearchContext", action.payload);
       return action.payload;
     case "RESET_SEARCH":
       return INITIAL_STATE;
@@ -26,7 +29,12 @@ export const SearchContextProvider = ({ children }) => {
 
   return (
     <SearchContext.Provider
-      value={{ city: state.city, dates: state.dates, options: state.options }}
+      value={{
+        city: state.city,
+        dates: state.dates,
+        options: state.options,
+        dispatch,
+      }}
     >
       {children}
     </SearchContext.Provider>
