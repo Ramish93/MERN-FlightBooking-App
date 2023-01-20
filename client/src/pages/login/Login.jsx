8add96ef4299bf3a64faa81c6191981734b2abb3
ramishshah93 <ramishshah93@gmail.com>
2023-01-20 16:31:33 +0100

    login page in making, custom css file along with it

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -12,24 +12,17 @@ const Login = () => {
 
   const { loading, error, dispatch } = useContext(AuthContext);
 
-  const navigate = useNavigate()
-
-  const handleChange = (e) => {
+  const handleChange = () => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
   const handleClick = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
-    try {
-      const res = await axios.post("/auth/login", credentials);
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
-      navigate("/")
-    } catch (err) {
-      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
-    }
-  };
 
+    try {
+    } catch (error) {}
+  };
 
   return (
     <div className="login">
@@ -48,7 +41,7 @@ const Login = () => {
           onChange={handleChange}
           className="lInput"
         />
-        <button disabled={loading} onClick={handleClick} className="lButton">
+        <button onClick={handleClick} className="lButton">
           Login
         </button>
         {error && <span>{error.message}</span>}
