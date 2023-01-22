00b48c05fa7f7393bb33303ea6c7c93a9d379643
ramishshah93 <ramishshah93@gmail.com>
2023-01-22 11:51:04 +0100

    fixed error reading properties of undefined: await was missing in mongo findById function

--- api/controllers/hotelControl.js
+++ api/controllers/hotelControl.js
@@ -94,12 +94,13 @@ export const countByType = async (req, res, next) => {
 
 export const getHotelRooms = async (req, res, next) => {
   try {
-    const hotel = Hotel.findById(req.params.id);
+    const hotel = await Hotel.findById(req.params.id);
     const list = await Promise.all(
       hotel.rooms.map((room) => {
         return Room.findById(room);
       })
     );
+    res.status(200).json(list);
   } catch (error) {
     next(error);
   }
